d9d50fd96285cb6624975d37bdbe5f53dbc5b56f
raquel <raquelalvesas@gmail.com>
2023-09-08 22:35:40 -0300

    ajustes

--- backend/src/main/java/br/com/mercadoalves/controle/domain/venda/DadosInformacoesCupom.java
+++
@@ -1,8 +0,0 @@
-package br.com.mercadoalves.controle.domain.venda;
-
-import java.math.BigDecimal;
-import java.sql.Date;
-import java.sql.Time;
-
-public record DadosInformacoesCupom (Date data, Time hora, BigDecimal valor, String caixa) {
-}
--- backend/src/main/java/br/com/mercadoalves/controle/domain/venda/DadosVenda.java
+++
@@ -1,7 +0,0 @@
-package br.com.mercadoalves.controle.domain.venda;
-
-import java.math.BigDecimal;
-import java.sql.Date;
-
-public record DadosVenda (String codigo, String produto, BigDecimal valor, String cpf, Date data, String cupom, String pagamento) {
-}
